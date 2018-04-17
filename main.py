43ad0160a35a9bc7cc593ff881f165105d19355b
thefakemacaw <38474041+thefakemacaw@users.noreply.github.com>
2018-04-17 11:45:30 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+''' 
+I was planning on making a fighting game, similar to Mortal Kombat, but after realizing the issues I had with 
+pygame and the fact that my original idea was too ambitious, I decided that I will make a meditation app with 
+built-in ambient/white noise. I will also incorporate aspects such as a play/pause button, mute button, and
+more.
+https://bcp.instructure.com/courses/3633/pages/course-resources
+'''
